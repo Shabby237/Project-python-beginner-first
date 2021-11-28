6d00762ae8c98550a3f59113f3929153f1fc37f6
Nalowa Geena <nalowageena654@gmail.com>
2021-11-28 11:24:17 +0100

    added textarea and label

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Text-to-Speech Converter")
 window.geometry('600x400')
+
+# creating widget
+label=tk.Label(window,text="Text", bg="blue", height=2,width=40)
+label.pack()
+# creating text widget
+T = tk.Text(window, height=20, width=40, bg = "white")
+T.pack(side=tk.LEFT)
+
 tk.mainloop()
+
+
